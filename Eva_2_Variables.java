7ac059bb96fa129ff397b447853c4d1c0b325d95
LeoAmparan10 <L24550381@chihuahua2.tecnm.mx>
2024-08-23 17:56:12 -0600

    Add files via upload

---
+++ Eva_2_Variables.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva_2_variables;
+
+/**
+ *
+ * @author invitado
+ */
+public class Eva_2_Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                                            //Tipos de datos
+        System.out.println("HOLA MUNDO!!"); //cadena de texto
+        System.out.println(100);            //Entero
+        System.out.println(3.1416);         //Numero con punto flotante
+        System.out.println('c');            //Caracter
+        System.out.println(true);           //Valores logicos(booleanos)
+        System.out.println(false);          //Valores logicos(booleanos)
+                                            //;-->Fin de estruccion
+    }
+    //Enteros:
+    //int
+    //long
+    //short
+    //NUMEROS CON PARTE DECIMAL(FLOTANTES:
+    //float
+    //double
+    //CADENA DE TEXTO:
+    //String
+    //Caracteres
+    //char
+    //VALORES LOGICOS:
+    //boolean
+    //DECLARACION DE VARIABLES
+    //SINTAXIS PARA DECLARAR VARIABLES(REGLAS)
+    //TIPO_DE_DATO NOMBRE_DE_LA_VARIABLE;
+    String nombre;
+    String apellido;
+    String curp;
+    float promedio;
+    
+ 
+}
